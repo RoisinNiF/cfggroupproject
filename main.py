4413544f4f2231a7bcb859ff269b4edf673c1b53
Doganf <82178633+Doganf@users.noreply.github.com>
2021-04-14 19:13:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def run():
         sales.append(sale)
     total = sum(sales)
     print('Total sales: {}'.format(total))
-    print('Average sales: {}'.format(total/12))
+    print('Average sales: {}'.format((round(total/12, 2))))
     month = []
     for row in data:
         months = str(row['month'])
@@ -31,7 +31,7 @@ def run():
     i = 0
     for sale in sales:
         percentage_diff=((sales[i+1] - sales[i])/sales[i]) * 100
-        print(percentage_diff)
+        print(round(percentage_diff, 2))
         i = i + 1
 run()
 
